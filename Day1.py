c9fa7f1f17968af8367ec6d55d3794f8aa358713
R4unak-t4 <thapamagarraunak@gmail.com>
2026-01-02 18:03:55 +0545

    First

--- Day1.py
+++ Day1.py
@@ -1,7 +1,46 @@
 # variable name can only start with a character or an underscore
-name = "thapa"
-print(name)
+# name = "thapa"
+# print(name)
 
 # 1var = "value" --> wrong ( variable name cannot start with numbers.)
 
+
+# Day 2
+
+# str
+name = "Raunak"
+# print(type(name))
+
+# int
+age = 20
+# print(type(age))
+
+# float
+# height = 174.0000
+# print(type(height))
+
+# bool
+# is_employed = False
+# print(type(is_employed))
+
+# num = 50
+# num1 = "2.1"
+# Arithmetic operators : +, -, *, /
+# can only be done for numeric values
+
+# +
+# print(num + num1)
+#
+# # -
+# print(num - num1)
+# # *
+# print(num * num1)
+# /
+# print(num / num1)
+
+
+first_name = "Raunak"
+last_name = "Thapa"
+
+print(first_name + " " + last_name)
 
